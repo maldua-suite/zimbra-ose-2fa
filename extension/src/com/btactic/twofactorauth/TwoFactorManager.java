7afc996375ad570288e9a99b41b543fb5851a235
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-05-23 20:58:20 +0200

    TwoFactorManager.java - Use Encoding from TwoFactorOptions instead.

--- extension/src/com/btactic/twofactorauth/TwoFactorManager.java
+++ extension/src/com/btactic/twofactorauth/TwoFactorManager.java
@@ -33,7 +33,7 @@ import com.zimbra.common.auth.twofactor.AuthenticatorConfig;
 import com.zimbra.common.auth.twofactor.TwoFactorOptions.CodeLength;
 import com.zimbra.common.auth.twofactor.TwoFactorOptions.HashAlgorithm;
 import com.zimbra.cs.account.auth.twofactor.CredentialConfig;
-import com.zimbra.cs.account.auth.twofactor.CredentialConfig.Encoding;
+import com.zimbra.common.auth.twofactor.TwoFactorOptions.Encoding;
 import com.zimbra.common.auth.twofactor.TOTPAuthenticator;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
