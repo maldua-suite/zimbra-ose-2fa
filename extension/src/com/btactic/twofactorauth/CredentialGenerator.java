c4da9a2bb59687de32620351fe34f54e4c71c816
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-05-26 18:10:59 +0200

    CredentialGenerator.java - Make sure to use the right package.

--- extension/src/com/btactic/twofactorauth/CredentialGenerator.java
+++ extension/src/com/btactic/twofactorauth/CredentialGenerator.java
@@ -17,7 +17,7 @@
  * If not, see <http://www.gnu.org/licenses/>.
  * ***** END LICENSE BLOCK *****
  */
-package com.zimbra.cs.account.auth.twofactor;
+package com.btactic.twofactorauth;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
