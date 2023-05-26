8873c4d7869a370eb87ac11911e47e5ada78237a
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-05-26 20:46:12 +0200

    ZetaTwoFactorAuthService.java - ZetaTwoFactorAuthService now extends AccountService

--- extension/src/com/btactic/twofactorauth/ZetaTwoFactorAuthService.java
+++ extension/src/com/btactic/twofactorauth/ZetaTwoFactorAuthService.java
@@ -6,12 +6,13 @@ package com.btactic.twofactorauth;
 
 import com.zimbra.soap.DocumentDispatcher;
 import com.zimbra.soap.DocumentService;
+import com.zimbra.cs.service.account.AccountService;
 
 import com.zimbra.common.soap.AccountConstants;
 
 import com.btactic.twofactorauth.soap.EnableTwoFactorAuth;
 
-public class ZetaTwoFactorAuthService implements DocumentService {
+public class ZetaTwoFactorAuthService extends AccountService implements DocumentService {
 
     public void registerHandlers(DocumentDispatcher dispatcher) {
         dispatcher.registerHandler(AccountConstants.ENABLE_TWO_FACTOR_AUTH_REQUEST, new EnableTwoFactorAuth());
