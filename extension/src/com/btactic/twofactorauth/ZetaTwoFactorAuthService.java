549e8c09b9e0c346bf85ac2384ec964c0bcc526e
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-05-31 16:17:13 +0200

    ZetaTwoFactorAuthService.java - Added ManyCalls Request was enabled.

--- extension/src/com/btactic/twofactorauth/ZetaTwoFactorAuthService.java
+++ extension/src/com/btactic/twofactorauth/ZetaTwoFactorAuthService.java
@@ -14,6 +14,12 @@ import com.btactic.twofactorauth.soap.EnableTwoFactorAuth;
 import com.btactic.twofactorauth.soap.DisableTwoFactorAuth;
 import com.btactic.twofactorauth.soap.CreateAppSpecificPassword;
 import com.btactic.twofactorauth.soap.RevokeAppSpecificPassword;
+import com.btactic.twofactorauth.soap.GetAppSpecificPasswords;
+import com.btactic.twofactorauth.soap.GetScratchCodes;
+import com.btactic.twofactorauth.soap.GenerateScratchCodes;
+import com.btactic.twofactorauth.soap.GetTrustedDevices;
+import com.btactic.twofactorauth.soap.RevokeTrustedDevice;
+import com.btactic.twofactorauth.soap.RevokeOtherTrustedDevices;
 
 public class ZetaTwoFactorAuthService extends AccountService implements DocumentService {
 
@@ -22,6 +28,12 @@ public class ZetaTwoFactorAuthService extends AccountService implements Document
         dispatcher.registerHandler(AccountConstants.DISABLE_TWO_FACTOR_AUTH_REQUEST, new DisableTwoFactorAuth());
         dispatcher.registerHandler(AccountConstants.CREATE_APP_SPECIFIC_PASSWORD_REQUEST, new CreateAppSpecificPassword());
         dispatcher.registerHandler(AccountConstants.REVOKE_APP_SPECIFIC_PASSWORD_REQUEST, new RevokeAppSpecificPassword());
+        dispatcher.registerHandler(AccountConstants.GET_APP_SPECIFIC_PASSWORDS_REQUEST, new GetAppSpecificPasswords());
+        dispatcher.registerHandler(AccountConstants.GET_SCRATCH_CODES_REQUEST, new GetScratchCodes());
+        dispatcher.registerHandler(AccountConstants.GENERATE_SCRATCH_CODES_REQUEST, new GenerateScratchCodes());
+        dispatcher.registerHandler(AccountConstants.GET_TRUSTED_DEVICES_REQUEST, new GetTrustedDevices());
+        dispatcher.registerHandler(AccountConstants.REVOKE_TRUSTED_DEVICE_REQUEST, new RevokeTrustedDevice());
+        dispatcher.registerHandler(AccountConstants.REVOKE_OTHER_TRUSTED_DEVICES_REQUEST, new RevokeOtherTrustedDevices());
     }
 
 }
