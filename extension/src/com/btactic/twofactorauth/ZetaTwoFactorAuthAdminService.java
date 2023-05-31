dadc520961b5a9878acd10df8e82691201167ff7
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-05-31 17:32:10 +0200

    ZetaTwoFactorAuthAdminService.java was added.

---
+++ extension/src/com/btactic/twofactorauth/ZetaTwoFactorAuthAdminService.java
@@ -0,0 +1,23 @@
+/*
+ * Zimbra OSE 2FA Administration zimlet
+ * Copyright (C) 2023 BTACTIC, S.C.C.L.
+ */
+package com.btactic.twofactorauth;
+
+import com.zimbra.soap.DocumentDispatcher;
+import com.zimbra.soap.DocumentService;
+import com.zimbra.cs.service.admin.AdminService;
+
+import com.zimbra.common.soap.AdminConstants;
+
+import com.btactic.twofactorauth.soap.ClearTwoFactorAuthData;
+import com.btactic.twofactorauth.soap.GetClearTwoFactorAuthDataStatus;
+
+public class ZetaTwoFactorAuthAdminService extends AdminService implements DocumentService {
+
+    public void registerHandlers(DocumentDispatcher dispatcher) {
+        dispatcher.registerHandler(AdminConstants.CLEAR_TWO_FACTOR_AUTH_DATA_REQUEST, new ClearTwoFactorAuthData());
+        dispatcher.registerHandler(AdminConstants.GET_CLEAR_TWO_FACTOR_AUTH_DATA_STATUS_REQUEST, new GetClearTwoFactorAuthDataStatus());
+    }
+
+}
