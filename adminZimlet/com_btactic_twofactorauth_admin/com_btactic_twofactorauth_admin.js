0bdde44638d18e63de8ddde136908ecbf2cfcf78
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-09-09 17:50:38 +0200

    adminZimlet proofOfConcept (2)

--- adminZimlet/com_btactic_twofactorauth_admin/com_btactic_twofactorauth_admin.js
+++ adminZimlet/com_btactic_twofactorauth_admin/com_btactic_twofactorauth_admin.js
@@ -21,6 +21,10 @@
 
 if(ZaSettings && ZaSettings.EnabledZimlet["com_btactic_twofactorauth_admin"]){
 
+    function com_btactic_twofactorauth_ext () {
+
+    }
+
     if (window.console && console.log) {
         console.log("Start loading com_btactic_twofactorauth_admin.js");
     }
@@ -28,6 +32,36 @@ if(ZaSettings && ZaSettings.EnabledZimlet["com_btactic_twofactorauth_admin"]){
     // Show additional 2FA attributes for Accounts
     if (window.ZaAccount && ZaAccount.myXModel && ZaAccount.myXModel.items) {
         ZaAccount.myXModel.items.push({id: "zimbraFeatureTwoFactorAuthAvailable", type: _COS_ENUM_, ref: "attrs/" + "zimbraFeatureTwoFactorAuthAvailable", choices: ZaModel.BOOLEAN_CHOICES});
+    }
+
+    if(ZaTabView.XFormModifiers["ZaAccountXFormView"]) {
+        com_btactic_twofactorauth_ext.AccountXFormModifier= function (xFormObject,entry) {
+            var cnt = xFormObject.items.length;
+            var i = 0;
+            for(i = 0; i <cnt; i++) {
+                if(xFormObject.items[i].type=="switch")
+                    break;
+            }
+            var tabBar = xFormObject.items[1] ;
+            var twofactorauthTabIx = ++this.TAB_INDEX;
+            tabBar.choices.push({value:twofactorauthTabIx, label:"TODO TWOFACTORAUTH TAB LABEL"});
+
+            var twofactorauthAccountTab={
+                type:_ZATABCASE_,
+                numCols:1,
+                caseKey:twofactorauthTabIx,
+                items: [
+                    {type:_ZAGROUP_,
+                        items:[
+                            {ref: "zimbraFeatureTwoFactorAuthAvailable", type: _SUPER_CHECKBOX_, checkBoxLabel: "TODO ENABLE 2FA LABEL", msgName: "TODO ENABLE 2FA LABEL", trueValue: "TRUE", falseValue: "FALSE", resetToSuperLabel: ZaMsg.NAD_ResetToCOS}
+                        ]
+                    }
+                ]
+            };
+
+            xFormObject.items[i].items.push(twofactorauthAccountTab);
+        }
+        ZaTabView.XFormModifiers["ZaAccountXFormView"].push(com_btactic_twofactorauth_ext.AccountXFormModifier);
     }
 
 }
