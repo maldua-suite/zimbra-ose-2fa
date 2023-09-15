f2a7e87cf02f673277da179dd7f17880c550bde8
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2023-09-15 19:28:52 +0200

    TwoFactorAuth QR zimlet: No need to check if the zimlet is enabled.

--- zimlet/com_btactic_twofactorauth_qr/com_btactic_twofactorauth_qr.js
+++ zimlet/com_btactic_twofactorauth_qr/com_btactic_twofactorauth_qr.js
@@ -23,54 +23,50 @@ if(window.console && window.console.log) {
     window.console.log("Starting loading com_btactic_twofactorauth_qr.js")
 }
 
-if(ZaSettings && ZaSettings.EnabledZimlet["com_btactic_twofactorauth_qr"]){
+// STEP 1. Force Preferences load so that we can override TwoFactor_all.js functions
+if (appCtxt.get(ZmSetting.MAIL_PREFERENCES_ENABLED)) {
+    AjxDispatcher.require(["PreferencesCore", "Preferences"]);
+}
 
-    // STEP 1. Force Preferences load so that we can override TwoFactor_all.js functions
-    if (appCtxt.get(ZmSetting.MAIL_PREFERENCES_ENABLED)) {
-        AjxDispatcher.require(["PreferencesCore", "Preferences"]);
+// STEP 3. Update QR when Next button is clicked.
+function twofactorauth_qr_setup() {
+
+    // Create QR Div if it is not there yet.
+    if (document.querySelectorAll('#twoFactorAuthQrDiv').length == 0) {
+        var emptyDivForTwoFactorAuth = document.createElement("div");
+        emptyDivForTwoFactorAuth.id = 'twoFactorAuthQrDiv';
+        document.querySelectorAll('.email-key')[0].after(emptyDivForTwoFactorAuth);
     }
 
-    // STEP 3. Update QR when Next button is clicked.
-    function twofactorauth_qr_setup() {
+    // Move the key to the left so that the QR can be shown
+    document.querySelectorAll('.email-key')[0].style.float="left";
 
-        // Create QR Div if it is not there yet.
-        if (document.querySelectorAll('#twoFactorAuthQrDiv').length == 0) {
-            var emptyDivForTwoFactorAuth = document.createElement("div");
-            emptyDivForTwoFactorAuth.id = 'twoFactorAuthQrDiv';
-            document.querySelectorAll('.email-key')[0].after(emptyDivForTwoFactorAuth);
-        }
-
-        // Move the key to the left so that the QR can be shown
-        document.querySelectorAll('.email-key')[0].style.float="left";
-
-        twofactorauth_next_button = $("[id$=" + '_button' + String(ZmTwoFactorSetupDialog.NEXT_BUTTON) + '_title' + "]")[0]
-        twofactorauth_next_button.addEventListener('click', () => {
-            $('#twoFactorAuthQrDiv')[0].innerHTML='';
-            var twofactorauth_qr_email = appCtxt.get(ZmSetting.USERNAME); // username@example.net
-            var twofactorauth_qr_secret = document.querySelectorAll('.email-key')[0].textContent;
-            var twofactorauth_qr_issuer = window.location.host; // mail.example.net
-            var qrcode = new QRCode($('#twoFactorAuthQrDiv')[0], {
-                text: "otpauth://totp/" + twofactorauth_qr_email + "?secret=" + twofactorauth_qr_secret + "&issuer=" + twofactorauth_qr_issuer  ,
-                width: 128,
-                height: 128,
-                colorDark : "#000000",
-                colorLight : "#ffffff",
-                correctLevel : QRCode.CorrectLevel.H
-            });
-
-        }
-        );
+    twofactorauth_next_button = $("[id$=" + '_button' + String(ZmTwoFactorSetupDialog.NEXT_BUTTON) + '_title' + "]")[0]
+    twofactorauth_next_button.addEventListener('click', () => {
+        $('#twoFactorAuthQrDiv')[0].innerHTML='';
+        var twofactorauth_qr_email = appCtxt.get(ZmSetting.USERNAME); // username@example.net
+        var twofactorauth_qr_secret = document.querySelectorAll('.email-key')[0].textContent;
+        var twofactorauth_qr_issuer = window.location.host; // mail.example.net
+        var qrcode = new QRCode($('#twoFactorAuthQrDiv')[0], {
+            text: "otpauth://totp/" + twofactorauth_qr_email + "?secret=" + twofactorauth_qr_secret + "&issuer=" + twofactorauth_qr_issuer  ,
+            width: 128,
+            height: 128,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        });
 
     }
-
-    // STEP 2. Default 2FA listener override with our Setup function
-    ZmTwoFactorSetupDialog.prototype._beginSetupButtonListener = (function(_super) {
-        return function() {
-            twofactorauth_qr_setup();
-            arguments[0] = arguments[0] - 1;
-            return _super.apply(this, arguments);
-        };
-
-    })(ZmTwoFactorSetupDialog.prototype._beginSetupButtonListener);
+    );
 
 }
+
+// STEP 2. Default 2FA listener override with our Setup function
+ZmTwoFactorSetupDialog.prototype._beginSetupButtonListener = (function(_super) {
+    return function() {
+        twofactorauth_qr_setup();
+        arguments[0] = arguments[0] - 1;
+        return _super.apply(this, arguments);
+    };
+
+})(ZmTwoFactorSetupDialog.prototype._beginSetupButtonListener);
